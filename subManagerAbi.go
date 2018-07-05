cfdc89a54dc558c09ee957113f27228e213c060f
David Samborski <bloggingarrow@gmail.com>
2018-07-04 23:10:27 -0500

    Correct import path

--- subManagerAbi.go
+++ subManagerAbi.go
@@ -7,12 +7,12 @@ import (
 	"math/big"
 	"strings"
 
-	ethereum "github.com/TxHash/go-ethereum"
-	"github.com/TxHash/go-ethereum/accounts/abi"
-	"github.com/TxHash/go-ethereum/accounts/abi/bind"
-	"github.com/TxHash/go-ethereum/common"
-	"github.com/TxHash/go-ethereum/core/types"
-	"github.com/TxHash/go-ethereum/event"
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
 )
 
 // SubscriptionManagerABI is the input ABI used to generate the binding from.
